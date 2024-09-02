b5ceacf7ba589f4a1da00b0793dd5c89bae3f67b
FilipMLipinski <72980477+FilipMLipinski@users.noreply.github.com>
2024-09-02 14:00:27 +0300

    added the Broker class

---
+++ Broker.py
@@ -0,0 +1,41 @@
+from Model import Action
+import requests
+
+class Broker():
+    def __init__(self, key_id, secret_key, url = "https://paper-api.alpaca.markets/v2/orders", \
+                 asset="BTC/USD", notional=10):
+        self.key_id = key_id
+        self.secret_key = secret_key
+        self.url = url
+        self.asset = asset
+        self.notional = notional # dollar amount to trade
+        self.headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "APCA-API-KEY-ID": str(key_id),
+            "APCA-API-SECRET-KEY": str(secret_key)
+        }
+        self.buy_payload = {
+            "side": "buy",
+            "type": "market",
+            "time_in_force": "gtc",
+            "symbol": asset,
+            "notional": notional
+        }
+        self.sell_payload = {
+            "side": "sell",
+            "type": "market",
+            "time_in_force": "gtc",
+            "symbol": asset,
+            "notional": notional          
+        }
+    def performAction(self, action : Action):
+        if(action == Action.BUY):
+            response = requests.post(self.url, json=self.buy_payload, headers=self.headers)
+            return response
+        elif(action == Action.SELL):
+            response = requests.post(self.url, json=self.sell_payload, headers=self.headers)
+            return response
+        elif(action == Action.SELL_ALL):
+            response = requests.delete(self.url, headers=self.headers)
+            return response
