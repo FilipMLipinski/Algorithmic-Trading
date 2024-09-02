d78ba60774dd837f56d639c1f484d68e59f14c88
FilipMLipinski <72980477+FilipMLipinski@users.noreply.github.com>
2024-09-02 14:00:46 +0300

    added main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # simple request to test the API
 import json
 import requests
-from Model import Model, Action
+from Model import Model, Action, Log
+from Broker import Broker
+import datetime
 
 APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
 
@@ -14,22 +16,35 @@ def readKeys(filename):
 
 def main():
     model = Model()
+    log = Log(str(datetime.datetime.now().month) + "_" + \
+              str(datetime.datetime.now().day) + "_" + \
+              str(datetime.datetime.now().hour) + "_" +  
+              str(datetime.datetime.now().minute))
     btcurl = "https://data.alpaca.markets/v1beta3/crypto/us/latest/quotes?symbols=BTC%2FUSD"
     keys = readKeys(".alpacakeys")
-    headers = {
+    broker = Broker(keys[0], keys[1])
+    infoHeaders = {
           "accept": "application/json",
           "APCA-API-KEY-ID": keys[0],
           "APCA-API-SECRET-KEY": keys[1]
     }
-    while(1):
-        response = requests.get(btcurl, headers=headers)
-        ask = response.json()['quotes']['BTC/USD']['ap']
-        bid = response.json()['quotes']['BTC/USD']['bp']
-        model.recordEvent(ask, bid)
-        action = model.getAction()
-        if(action != Action.NO_ACTION):
-            print(model.reference)
-            print(action)
+    try:
+        while(1):
+            response = requests.get(btcurl, headers=infoHeaders)
+            ask = response.json()['quotes']['BTC/USD']['ap']
+            bid = response.json()['quotes']['BTC/USD']['bp']
+            model.recordEvent(ask, bid)
+            action = model.getAction()
+            if(action != Action.NO_ACTION):
+                broker_response = broker.performAction(action)
+                print(model.reference)
+                print(action)
+                print(broker_response.text)
+            log.recordEvent(ask, bid, action)
+    except KeyboardInterrupt:
+        broker_response = broker.performAction(Action.SELL_ALL)
+        print(broker_response.text)
+
 
 if __name__ == "__main__":
     main()
